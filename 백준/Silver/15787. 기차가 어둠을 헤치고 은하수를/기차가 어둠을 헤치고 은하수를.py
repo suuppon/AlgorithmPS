932cc3d2836308e769a86239ebf5d941529cd13e
Junsub Kim <80026532+suuppon@users.noreply.github.com>
2025-05-07 18:13:05 +0900

    [Silver II] Title: 기차가 어둠을 헤치고 은하수를, Time: 364 ms, Memory: 109720 KB -BaekjoonHub

--- 백준/Silver/15787. 기차가 어둠을 헤치고 은하수를/기차가 어둠을 헤치고 은하수를.py
+++ 백준/Silver/15787. 기차가 어둠을 헤치고 은하수를/기차가 어둠을 헤치고 은하수를.py
@@ -1,31 +1,33 @@
-N,M = map(int, input().split())
+import sys
+input = sys.stdin.readline
+from collections import deque
 
-#M번의 명령 수 받아서 명령들을 List에 저장
-order = list(tuple(map(int, input().split())) for _ in range(M))
+N, M = map(int, input().split())
 
-#원소가 모두 0인 N by 20 데이터프레임 생성해 기차 표현
-df = [[0 for _ in range(20)] for _ in range(N)]
+train = [deque([0] * 20) for _ in range(N + 1)]
 
-#명령 실행
-for j in range(M):
-    if order[j][0] == 1:
-        df[order[j][1]-1][order[j][2]-1] = 1
-    elif order[j][0] == 2:
-        df[order[j][1]-1][order[j][2]-1] = 0
-    elif order[j][0] == 3:
-        df[order[j][1]-1].insert(0,0)
-        df[order[j][1]-1].pop(-1)
+for idx in range(M):
+    cmd, *args = map(int, input().split())
+    if cmd in (1, 2):
+        i, x = args
     else:
-        df[order[j][1]-1].pop(0)
-        df[order[j][1]-1].append(0)
-
-# 결과를 저장할 빈 집합 생성
-my_set = set()
-
-# 리스트 내의 각 리스트를 검사하여 집합에 추가
-for sublist in df:
-    # 리스트가 모두 0으로 이루어져 있지 않은 경우에만 집합에 추가
-    my_set.add(tuple(sublist))
-
-# 결과 확인
-print(len(my_set))
+        i = args[0]
+        
+    if cmd == 1:
+        train[i][x-1] = 1
+        
+    elif cmd == 2:
+        train[i][x-1] = 0
+        
+    elif cmd == 3:
+        train[i].appendleft(0)
+        train[i].pop()
+        
+    elif cmd == 4:
+        train[i].append(0)
+        train[i].popleft()
+        
+answer = set()
+for i in range(1, N + 1):
+    answer.add(tuple(train[i]))
+print(len(answer))
