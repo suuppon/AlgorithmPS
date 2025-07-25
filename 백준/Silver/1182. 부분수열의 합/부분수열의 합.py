6727de75c166e869ff1a3806d5dba52c7b7dac64
Junsub Kim <80026532+suuppon@users.noreply.github.com>
2025-07-25 18:15:02 +0900

    [Silver II] Title: 부분수열의 합, Time: 236 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -2,21 +2,20 @@ N, S = map(int, input().split())
 sequence = list(map(int, input().split()))
 sequence.sort()
 
-count = [0]
+count = 0
 
 def backtrack(idx, current_sum):
+    global count
     if idx >= N:
         return
 
     current_sum += sequence[idx]
 
     if current_sum == S:
-        count[0] += 1
-
-    # 현재 원소 포함 O
+        count += 1
+        
     backtrack(idx + 1, current_sum)
-    # 현재 원소 미포함 X
-    backtrack(idx + 1, current_sum - sequence[idx])  # 다시 빼줘야 원상복구됨
+    backtrack(idx + 1, current_sum - sequence[idx])
 
 backtrack(0, 0)
-print(count[0])
+print(count)
